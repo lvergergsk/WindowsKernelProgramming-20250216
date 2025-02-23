09f8a65166c9d8c472ff27514da9a8cf65bc4b69
youlyu.zhang <lvergergsk@gmail.com>
2025-02-23 22:33:59 +0900

    update 20250223

---
+++ WindowsKernelProgramming006/WindowsKernelProgramming006.cpp
@@ -0,0 +1,31 @@
+#include <ntddk.h>
+
+VOID UnloadDriver(PDRIVER_OBJECT DriverObject);
+
+extern "C" NTSTATUS DriverEntry(_In_ PDRIVER_OBJECT DriverObject, _In_ PUNICODE_STRING RegistryPath)
+{
+    PVOID allocatedMemory = NULL;
+    const char* message = "Hello, Kernel World!";
+
+    UNREFERENCED_PARAMETER(RegistryPath);
+    DriverObject->DriverUnload = UnloadDriver;
+
+    allocatedMemory = ExAllocatePool2(POOL_FLAG_NON_PAGED, 1024, 'Mem1');
+    if (allocatedMemory == NULL)
+    {
+        DbgPrint("Memory allocation failed\n");
+        return STATUS_INSUFFICIENT_RESOURCES;
+    }
+
+    RtlCopyMemory(allocatedMemory, message, strlen(message) + 1);
+    DbgPrint("Memory content: %s\n", (char*)allocatedMemory);
+
+    ExFreePoolWithTag(allocatedMemory, 'Mem1');
+    return STATUS_SUCCESS;
+}
+
+VOID UnloadDriver(PDRIVER_OBJECT DriverObject)
+{
+    KdPrint(("006 UnloadDriver\n"));
+    IoDeleteDevice(DriverObject->DeviceObject);
+}
